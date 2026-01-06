27fe5e44967b5c11e3501f9cc454026d26c17e12
sendyhtml <sendyfalisha@gmail.com>
2026-01-06 16:38:37 +0700

    Add create.php

---
+++ create.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>TOOLS CREATE JASTEB 👑</title>
+</head>
+<body>
+<div class="panel">
+    <form action="proses.php" method="post">
+        <label for="nomor"><br>😈𝐂𝐑𝐄𝐀𝐓𝐄 𝐉𝐀𝐒𝐓𝐄𝐁😈
+        </br>
+        <br>
+        𝗖𝗥𝗔𝗧𝗘 𝗪𝗘𝗕 𝗖𝗨𝗥𝗟 𝗦𝗘𝗦𝗨𝗔𝗜 𝗡𝗔𝗠𝗔 𝗝𝗔𝗦𝗧𝗘𝗕 𝗬𝗔𝗡𝗚 𝗗𝗜𝗕𝗘𝗥𝗜𝗞𝗔𝗡
+        </br>
+        <br>
+        1. PANEL JASTEB
+        </br>
+        </label>
+        <input type="text" id="text" name="text" min="1" max="9999" required>
+        <div class="button-group">
+            <button type="submit" name="cekweb">𝗖𝗲𝗸 𝗧𝗮𝗺𝗽𝗶𝗹𝗮𝗻</button>
+            <button type="submit" name="buatweb">𝗕𝘂𝗮𝘁 𝗪𝗲𝗯</button>
+        </div>
+    </form>
+</div>
+
+</body>
+</html>
