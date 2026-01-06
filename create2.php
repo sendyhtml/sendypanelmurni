83ac8dd69f6966f02b82a72a3680abd34a9ab35b
sendyhtml <sendyfalisha@gmail.com>
2026-01-06 16:38:39 +0700

    Add create2.php

---
+++ create2.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>TOOLS CREATE JASTEB⚡</title>
+</head>
+<body>
+<div class="panel">
+    <form action="proses2.php" method="post">
+        <label for="nomor"><br>😈𝐂𝐑𝐄𝐀𝐓𝐄 𝐉𝐀𝐒𝐓𝐄𝐁😈:
+        </br>
+        <br>
+        𝟭.𝗝𝗔𝗦𝗧𝗘𝗕 SENDYHOST [𝟭]🔥
+        </br>
+        <br>
+        𝟮.𝗝𝗔𝗦𝗧𝗘𝗕 SENDYHOST [𝟮]🔥
+        </br>
+        <br>
+       𝗖𝗘𝗞 𝗧𝗔𝗠𝗣𝗜𝗟𝗔𝗡 𝗨𝗡𝗧𝗨𝗞 𝗠𝗘𝗟𝗜𝗛𝗔𝗧 𝗖𝗢𝗡𝗧𝗢𝗛
+        </br>
+        <br>
+        </label>
+        <input type="number" id="nomor" name="nomor" min="1" max="2" required>
+        <div class="button-group">
+            <button type="submit" name="cekweb">𝗖𝗲𝗸 𝗧𝗮𝗺𝗽𝗶𝗹𝗮𝗻</button>
+            <button type="submit" name="buatweb">𝗕𝘂𝗮𝘁 𝗝𝗮𝘀𝘁𝗲𝗯</button>
+        </div>
+    </form>
+</div>
+
+</body>
+</html>
